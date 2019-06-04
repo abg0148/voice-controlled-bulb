cc21d312bef6692c5253f01a17ac8c3365585e1d
Abhinav Gupta <abhinavgupta0148@gmail.com>
2019-06-04 14:23:06 +0530

    Add files via upload

---
+++ ignore.ino
@@ -0,0 +1,41 @@
+String readString;
+#define ledPin 13
+void setup() {
+  pinMode(ledPin, OUTPUT);
+  digitalWrite(ledPin, LOW);
+  digitalWrite(6,HIGH);
+  Serial.begin(9600);
+  Serial.println("serial-delimit-21"); // so I can keep track of what is loaded
+}
+
+void loop() {
+  while (Serial.available()) {
+    delay(10);  //small delay to allow input buffer to fill
+
+    char c = Serial.read();  //gets one byte from serial buffer
+    if (c == ',') {
+      break;
+    }  //breaks out of capture loop to print readstring
+    readString += c; 
+  } //makes the string readString  
+
+  if (readString.length() >0) {
+    Serial.println(readString); //prints string to serial port out
+    if(readString=="w"){
+      digitalWrite(ledPin,HIGH);
+      }
+     if(readString=="s"){
+      digitalWrite(ledPin,LOW);
+      }
+     if(readString=="switch on"){
+      digitalWrite(ledPin,HIGH);
+      }
+     if(readString=="switch off"){
+      digitalWrite(ledPin,LOW);
+      }
+    readString=""; //clears variable for new input
+  }
+
+  
+}
+
---
+++ ignore/digicom.ino
@@ -0,0 +1,20 @@
+#define ledPin 13
+String state = "0";
+void setup() {
+  pinMode(ledPin, OUTPUT);
+  digitalWrite(ledPin, LOW);
+  Serial.begin(9600); // Default communication rate of the Bluetooth module
+}
+void loop() {
+  if(Serial.available() > 0){ // Checks whether data is comming from the serial port
+    String state = Serial.read(); // Reads the data from the serial port
+    Serial.println(state);
+    delay(10);
+    if(state=="w"){
+      digitalWrite(ledPin,HIGH);
+      }
+   if(state=="s"){
+      digitalWrite(ledPin,LOW);
+      }
+ } 
+}
